7af11c24eab9b347c0dd4fdb101af0a3b693c55a
Edwyn <edwyn__4@hotmail.com>
2023-12-03 22:22:45 +0100

    Now when you enter a city or country, it displays the weather data. So, once place is entered and searched, the weather class block is overriden and displays the weather data.

--- weather.js
+++ weather.js
@@ -9,8 +9,6 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
-
     document.querySelector('.weather-city').innerHTML = data.name;
     document.querySelector('.weather-temp').innerHTML = Math.round(data.main.temp) + '°C';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
@@ -33,6 +31,8 @@ async function checkWeather(city) {
     } else if (data.weather[0].main == "Wind") {
         weatherIcon.src = "images/wind.png";
     }
+
+    document.querySelector('.weather').style.display = 'block';
 }
 
 searchBtn.addEventListener('click', ()=> {
